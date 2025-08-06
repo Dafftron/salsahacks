069e61453a92b91a70604de998fef22410d0ed7b
David SalsaHacks <david@salsahacks.com>
2025-08-06 14:46:46 +0200

    REVERTIDO - VideoCombiner a fetch para arreglar CORS
     CAMBIOS:
    - Revertido VideoCombiner para usar fetch nuevamente
    - Eliminadas dependencias de Firebase Storage directo
    - Preparado para configuración CORS correcta
    
     PRÓXIMO PASO:
    - Configurar CORS en Firebase Storage
    - Usar gsutil o consola web de Firebase
    
     OBJETIVO: Descarga funcional sin errores CORS

--- src/services/video/videoCombiner.js
+++ src/services/video/videoCombiner.js
@@ -1,5 +1,5 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg'
-import { fetchFile, toBlobURL } from '@ffmpeg/util'
+import { getDownloadURL, ref } from 'firebase/storage'
+import { storage } from '../firebase/config'
 
 class VideoCombiner {
   constructor() {
