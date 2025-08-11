e7fb37b7f0f97e9a93a23491566ca1fc98d36543
Dafftron <dafftron@gmail.com>
2025-08-11 17:24:09 +0200

    Limpieza inicial: eliminar archivos basura en raíz, quitar DownloadModal duplicado no usado, ignorar y eliminar dist/

--- src/components/common/DownloadModal.jsx
+++
@@ -1,4 +0,0 @@
-import React, { useState } from 'react'
-// TEMPORALMENTE COMENTADO PARA DIAGNÓSTICO
-// import { convertVideoFormat } from '../../services/video/videoProcessor'
-import { Download, X, CheckCircle, AlertCircle } from 'lucide-react' 
